d786e3b617b868c08c21bb261bb576de9bb73d4a
Rakesh Gain <rakeshgain47681@gmail.com>
2022-08-26 16:41:59 +0530

    working fine

--- temp.py
+++ temp.py
@@ -1,5 +1,5 @@
 import urllib.request
 import os
 
-urllib.request.urlretrieve("https://c.ndtvimg.com/2020-07/m7opt04g_ms-dhoni-afp_625x300_06_July_20.jpg", "./static/Images/virat.png")
+urllib.request.urlretrieve("https://res.cloudinary.com/du64hfpln/image/upload/v1661510179/samples/WhatsApp_Image_2022-08-26_at_3.58.41_PM_bj8nx5.jpg", "./static/Images/rakesh.png")
 # os.remove("./static/Images/GL.png")
